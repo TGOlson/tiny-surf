73c72236fee91a226c66271edeb7562344a7c55f
Tyler Olson <tydotg@gmail.com>
2023-02-13 11:47:32 -0800

    store selection in reducer

---
+++ src/app/components/SpotList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Spot } from '../../shared/types';
+import { useAppDispatch } from '../hooks';
+import { spotSelected } from '../spot-reducer';
+
+type Params = {
+  spots: Spot[],
+  selected: Spot | null;
+};
+
+const SpotList = ({spots, selected}: Params) => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="spot-list">
+      <ul>
+        {spots.map(spot => (
+          <li key={spot.id}>
+            <Link 
+              to={"../" + spot.slug} 
+              onClick={() => dispatch(spotSelected(spot.slug))} 
+              relative="path"
+              className={selected?.id === spot.id ? 'selected' : ''}
+            >
+              {spot.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SpotList;
--- src/app/pages/SpotExplorer.tsx
+++ src/app/pages/SpotExplorer.tsx
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import SpotList from '../components/SpotList';
 
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { fetchSpots, spotSelected } from '../spot-reducer';
 
 const SpotExplorer = () => {
+  const params = useParams();
+  const paramSlug = params.slug;
+
+  if (!paramSlug) throw new Error('Unexpected url params');
+
   const dispatch = useAppDispatch();
   const spotsData = useAppSelector(st => st.spot.spots);
+  const selected = useAppSelector(st => st.spot.selected);
 
   useEffect(() => {
     if (spotsData.status === 'idle') void dispatch(fetchSpots());
   }, [spotsData.status, dispatch]);
 
+  useEffect(() => {
+    if (!selected) dispatch(spotSelected(paramSlug));
+  }, [selected, dispatch]);
+  
   if (spotsData.status === 'idle' || spotsData.status === 'pending' ) {
     return <p>Loading...</p>;
   }
@@ -19,24 +30,21 @@ const SpotExplorer = () => {
   if (spotsData.status === 'rejected') {
     return <p>Error: {spotsData.error}</p>;
   }
-
+  
   const spots = spotsData.data;
+
+  const selectedSpot = spots.find(spot => spot.slug === paramSlug);
+  
+  if (!selectedSpot) {
+    return <p>Unable to find spot {paramSlug}</p>;
+  }
+
+  const location = selectedSpot.locationNamePath.slice(2).join(' ');
 
   return (
     <div className="spot-list">
-      <ul>
-        {spots.map(spot => (
-          <li key={spot.id}>
-            <NavLink 
-              to={"../" + spot.slug} 
-              onClick={() => dispatch(spotSelected(spot.slug))} 
-              relative="path"
-            >
-              {spot.name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+      <h4>{location}</h4>
+      <SpotList spots={spots} selected={selectedSpot} />
     </div>
   );
 };
