0bdaae6360846b1689c336973787cae935f7e34e
Tyler Olson <tydotg@gmail.com>
2023-02-16 14:04:00 -0800

    add chart.js

--- src/app/components/SpotInfo.tsx
+++ src/app/components/SpotInfo.tsx
@@ -1,11 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Spot } from '../../shared/types';
+import { Forecast, Spot } from '../../shared/types';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { fetchForecast } from '../slices/forecast-slice';
 
+import { Line } from 'react-chartjs-2';
+
+// little hack to register everything from chart.js
+// alternative is to use `.register` on ever component we use
+import 'chart.js/auto';
+import 'chartjs-adapter-luxon';
+
 type Params = {
   spot: Spot;
+};
+const timeString = (ts: number): string => {
+  // TODO: use utc offset
+  const date = new Date(ts);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  return `${hours > 12 ? hours - 12 : hours}:${minutes}${hours >= 12 ? 'pm' : 'am'}`;
+};
+
+const TideChart = ({tides}: {tides: Forecast['data']['tides']}) => {
+
+
+  const data = {
+    datasets: [{
+      data: tides.map(tide => ({x: tide.timestamp, y: tide.height})),
+    }]
+  };
+
+  return (
+    <Line 
+      data={data} 
+      options={{
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              // Luxon format string
+              tooltipFormat: 't',
+            },
+          },
+        },
+        plugins: {
+          legend: {
+              display: false,
+          },
+        }
+      }} 
+    />
+  );
 };
 
 const SpotInfo = ({spot}: Params) => {
@@ -25,17 +72,20 @@ const SpotInfo = ({spot}: Params) => {
     return <p>Error: {forecast.error}</p>;
   }
 
+  const {data, units} = forecast.data;
+
   return (
     <div>
       <p>{spot.name}</p>
-      <p>Rating: {forecast.data.data.ratings[0]?.key}</p>
-      <p>Wave height: {forecast.data.data.waves[0]?.min}-{forecast.data.data.waves[0]?.max} {forecast.data.units.waveHeight.toLowerCase()}.</p>
-      <p>Wind: {forecast.data.data.wind[0]?.speed} {forecast.data.units.windSpeed.toLowerCase()}.</p>
+      <p>Rating: {data.ratings[0]?.key}</p>
+      <p>Wave height: {data.waves[0]?.min}-{data.waves[0]?.max} {units.waveHeight.toLowerCase()}.</p>
+      <p>Wind: {data.wind[0]?.speed} {units.windSpeed.toLowerCase()}.</p>
+      <TideChart tides={data.tides.slice(0, 23)}/>
       <p>Tides:</p>
       <ul>
         {forecast.data.data.tides.filter(x => x.type !== 'NORMAL').map(tide => {
           return (
-            <li key={tide.timestamp}>Tide: <span>{tide.type}</span> at <span>{(new Date(tide.timestamp * 1000)).toLocaleString()}</span></li>
+            <li key={tide.timestamp}>Tide: <span>{tide.type}</span> at <span>{(new Date(tide.timestamp)).toLocaleString()}</span></li>
           );
         })}
       </ul>
